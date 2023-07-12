72e3c4ea9c8bc41e5aaa2259f63616a7ac8e0423
fcrespo8 <fcrespo8@hotmail.com>
2023-07-12 11:15:16 +0200

    blog models migrated and fixed

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from django.utils.timezone import now
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -20,7 +20,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
     content = models.TextField(verbose_name="Contenido")
-    published = models.DateTimeField(verbose_name="Fecha de publicación", default=timezone.now)
+    published = models.DateTimeField(verbose_name="Fecha de publicación", default=now)
     image = models.ImageField(verbose_name="Imagen", upload_to="blog", null=True, blank=True)
     author = models.ForeignKey(User, verbose_name="Autor", on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category, verbose_name="Categorías")
